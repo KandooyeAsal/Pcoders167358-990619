5d822804c5de818bda998bb6030dda4dfe9afbab
amir hosein <a.h.keshavarz74@gmail.com>
2020-09-10 14:47:40 +0430

    little changes

--- Cases_func.m
+++ Cases_func.m
@@ -5,6 +5,7 @@ global P
 dis = abs(P.deltaSigma_acc - deltaSigma);
 [~, Ind_min] = min(dis, [], 2);
 
+%% plotting
 figure;
 plot(real(P.deltaSigma_acc.'),imag(P.deltaSigma_acc.'), '-x')
 grid on; title('test ball curve'); xlabel('real(detlaSigma)'); ylabel('imag(deltaSigma)');
@@ -14,6 +15,7 @@ plot(real(deltaSigma), imag(deltaSigma), 'kh', 'MarkerSize',15,'MarkerEdgeColor'
 for i = 1:size(P.deltaSigma_acc, 1)
     plot(real(P.deltaSigma_acc(i, Ind_min(i))), imag(P.deltaSigma_acc(i, Ind_min(i))), 'cp','MarkerSize',10,'MarkerEdgeColor','c','MarkerFaceColor',[0.5,0.5,0.5])
 end
+%%
 
 for i = 1:size(P.deltaSigma_acc, 1)
     ambiguty(i) = P.logic_ind_acc(i, Ind_min(i));
@@ -31,5 +33,4 @@ if sum(ambiguty) < size(P.deltaSigma_acc, 1)
 elseif sum(ambiguty) == size(P.deltaSigma_acc, 1)
     thetaEst_final = P.thetaEst_acc(size(P.deltaSigma_acc, 1), Ind_min(size(P.deltaSigma_acc, 1)));
 end
-
 end
